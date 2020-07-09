d9628b17e102e2c14fd01c99b4eca7b45e380bc6
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-07-09 16:36:38 -0700

    updated the reducer in the reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,5 @@
 let lastId = 0;
-function reducer(state, action) {
+export default function reducer(state = [], action) { // set state to a default [], this is called a default argument.
   if (action.type === 'bugAdded') {
     return [
       ...state, //spread operator ... makes a shallow copy of the state ojbect.
@@ -7,9 +7,13 @@ function reducer(state, action) {
       // to see how to insert an object in the middle of an array see notes in google docs REDUX in the section on "Immutability working with arrays"
       {
         id: ++lastId, //subs the last id, adds 1 then assigs the lastId var to the id property
-        description: action.payload.description.action,
+        description: action.payload.description,
         resolved: false
       }
     ]
+  } else if (action.type ==='bugRemoved') {
+    return state.filter(bug => bug !== action.payload.id); //filter removes the item and returns an array without that item.
+  } else {
+    return state;
   }
 }
