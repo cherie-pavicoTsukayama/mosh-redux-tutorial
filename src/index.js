7b8182f2337cf6150ab28ab1c00ca3181dff5cb8
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-07-09 16:58:59 -0700

    subscribing and unsubscribing from the store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import store from './store';
 
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed!", store.getState());
+  //this is where you would put
+  //the code for updating the dom if you're working with vanilla javascript.
+  //Or rerender if you're working in react.
+})
+
 store.dispatch({
   type: "bugAdded",
   payload: {
     description: "Bug1"
   }
 })
+
+unsubscribe();
 
 store.dispatch({
   type: "bugRemoved",
